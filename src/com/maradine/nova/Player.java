4f21893f81145c4fb8dde648f3d7a50cd982c738
maradine <maradine@gmail.com>
2014-07-10 15:55:43 -0700

    Added Player

---
+++ src/com/maradine/nova/Player.java
@@ -0,0 +1,23 @@
+package com.maradine.nova;
+
+/**
+ * Created by cdeibler on 7/10/14.
+ */
+public class Player {
+
+    private final int id;
+    private final String name;
+
+    public Player(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
